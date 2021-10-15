b0b9484c6f8df1f0b90cb46540597cd8c79be75b
negativefeedback <negativefeedback@protonmail.com>
2021-10-15 16:22:15 +0000

    add example terraform boundary creator depends on nomad

---
+++ boundary-tf/provider.tf
@@ -0,0 +1,7 @@
+provider "boundary" {
+  addr                            = "https://boundary.service.consul:9200"
+  auth_method_id                  = "ampw_KqDvc3DmB4"
+  password_auth_method_login_name = "admin"
+  password_auth_method_password   = ""
+}
+
